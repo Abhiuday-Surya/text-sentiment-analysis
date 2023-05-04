de28c77e71040a55462eb61e3990aeeec02b7486
Abhiuday-Surya <93286245+Abhiuday-Surya@users.noreply.github.com>
2023-05-04 22:32:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 import sentiment from 'sentiment';
-var sentimentobj = new sentiment();
-var docx = sentimentobj.analyze("i like mango ");
-var com = docx.comparative;
-document.getElementById("myBtn").addEventListener("click",showMsg);
 
-function showMsg(){
-alert("hello world");
+const sentimentObj = new sentiment();
+
+const docx = sentimentObj.analyze('i like mango');
+const com = docx.comparative;
+
+function showMsg() {
+  alert(`Sentiment score: ${com}`);
 }
+
+document.getElementById('myBtn').addEventListener('click', showMsg);
