a83e98edbeecad3bb9af242805cc340425a3db67
abhiuday <suryaabhiuday@gmail.com>
2023-05-04 19:29:30 -0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var sentiment = require('sentiment');
+import sentiment from 'sentiment';
 var sentimentobj = new sentiment();
 var docx = sentimentobj.analyze("i like mango ");
 var com = docx.comparative;
-console.log(com);
+document.getElementById("myBtn").addEventListener("click",showMsg);
+
+function showMsg(){
+alert("hello world");
+}
